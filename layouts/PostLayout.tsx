4f826b2e8a0e14bf5148662bcaaaca4d15dbfd93
olchikd <olchikd@gmail.com>
2025-03-12 09:26:00 +0300

    Updating Post details: authors section

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -61,7 +61,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               <dd>
                 <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-y-8 xl:space-x-0">
                   {authorDetails.map((author) => (
-                    <li className="flex items-center space-x-2" key={author.name}>
+                    <div key={author.name} className="flex items-center space-x-2">
                       {author.avatar && (
                         <Image
                           src={author.avatar}
@@ -71,24 +71,16 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                           className="h-10 w-10 rounded-full"
                         />
                       )}
-                      <dl className="text-sm leading-5 font-medium whitespace-nowrap">
-                        <dt className="sr-only">Name</dt>
-                        <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.twitter && (
-                            <Link
-                              href={author.twitter}
-                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            >
-                              {author.twitter
-                                .replace('https://twitter.com/', '@')
-                                .replace('https://x.com/', '@')}
-                            </Link>
-                          )}
-                        </dd>
-                      </dl>
-                    </li>
+                      <div className="text-sm leading-5">
+                        <Link
+                          href={author.linkedin}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 font-medium"
+                        >
+                          {author.name}
+                        </Link>
+                        <p className="text-gray-600 dark:text-gray-400">{author.occupation}</p>
+                      </div>
+                    </div>
                   ))}
                 </ul>
               </dd>
