34845f392d4efcb88ad8dd0e3a9b0e270e469c51
olchikd <olchikd@gmail.com>
2025-03-12 08:22:56 +0300

    Fix in margin

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,3 +1,3 @@
 export default function PagesLayout({ children }: { children: React.ReactNode }) {
-  return <div className="px-12 text-justify">{children}</div>
+  return <div className="p-12 text-justify">{children}</div>
 }
