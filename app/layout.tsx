99cdefb9cb09b44347605cb28753eccaf7ba84da
olchikd <olchikd@gmail.com>
2025-03-15 09:27:51 +0300

    Adding Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,15 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-space-grotesk',
 })
+
+const env = process.env.NODE_ENV
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
@@ -93,6 +96,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
+      {env == 'production' && <GoogleTagManager gtmId="GTM-WH3KZ5KX" />}
       <body className="bg-gray-100 text-black antialiased">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
