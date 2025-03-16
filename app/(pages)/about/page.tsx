31eca4e8a7e0a4e64871eae13105dd89d0103eee
olchikd <olchikd@gmail.com>
2025-03-16 11:42:53 +0300

    Adding WebPage and WebSite JsonLd

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -3,8 +3,29 @@ import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from '../../seo'
+import WebPageJsonLd from '@/components/jsonLd/WebPageJsonLd'
 
-export const metadata = genPageMetadata({ title: 'About' })
+const title = 'About'
+
+const description = `
+    At StabRise, we’re reshaping how businesses manage and process their documents. We specialize in handling structured and unstructured data with a focus on security and compliance. Our flexible, scalable solutions help businesses meet the highest standards in document processing, including HIPAA, GDPR, and custom privacy requirements.
+  `
+const keywords = [
+  'document processing',
+  'data security',
+  'HIPAA compliance',
+  'GDPR compliance',
+  'open source',
+  'data de-identification',
+  'AWS',
+  'GCP',
+  'document management',
+  'structured data',
+  'unstructured data',
+  'document automation',
+]
+
+export const metadata = genPageMetadata({ title: title })
 
 export default function Page() {
   const author = allAuthors.find((p) => p.slug === 'default') as Authors
@@ -12,6 +33,12 @@ export default function Page() {
 
   return (
     <>
+      <WebPageJsonLd
+        pageUrl="/about/"
+        title={title}
+        description={description}
+        keywords={keywords}
+      />
       <AuthorLayout content={mainContent}>
         <MDXLayoutRenderer code={author.body.code} />
       </AuthorLayout>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,42 +5,66 @@ import Image from '@/components/Image'
 import SectionContainer from '@/components/SectionContainer'
 import PageTitle from '@/components/PageTitle'
 import H3 from '@/components/H3'
+import WebPageJsonLd from '@/components/jsonLd/WebPageJsonLd'
 
-export const metadata = genPageMetadata({ title: 'Projects' })
+const title = 'Our Projects'
+const keywords = [
+  'open-source projects',
+  'Apache Spark',
+  'Spark PDF',
+  'Scale DP',
+  'PDF redaction',
+  'data de-identification',
+  'AI-powered document processing',
+  'HIPAA compliance',
+  'GDPR compliance',
+  'document automation',
+  'scalable data processing',
+]
+
+export const metadata = genPageMetadata({ title: title })
 
 export default function Projects() {
   return (
-    <div className="pb-12">
-      <PageTitle>Our Projects</PageTitle>
-      <div className="space-y-8">
-        {projectsData.map((d) => (
-          <SectionContainer key={d.title} addMinHeight={false}>
-            <Link href={d.href} aria-label={`Link to ${d.title}`}>
-              <div className="m-3 p-6">
-                <div className="flex flex-col items-center sm:flex-row sm:items-center sm:space-y-0 sm:space-x-6">
-                  {d.img2Src ? (
-                    <div className="flex-shrink-0 sm:flex-none">
-                      <Image
-                        alt={d.title}
-                        src={d.img2Src}
-                        className="h-32 w-32 rounded-lg object-cover"
-                        width={54}
-                        height={54}
-                      />
+    <>
+      <WebPageJsonLd
+        pageUrl="/projects/"
+        title={title}
+        description="Explore our open-source projects designed to help businesses process documents efficiently and securely using Apache Spark. From PDF handling to data de-identification, our solutions are scalable and compliant with privacy regulations."
+        keywords={keywords}
+      />
+      <div className="pb-12">
+        <PageTitle>Our Projects</PageTitle>
+        <div className="space-y-8">
+          {projectsData.map((d) => (
+            <SectionContainer key={d.title} addMinHeight={false}>
+              <Link href={d.href} aria-label={`Link to ${d.title}`}>
+                <div className="m-3 p-6">
+                  <div className="flex flex-col items-center sm:flex-row sm:items-center sm:space-y-0 sm:space-x-6">
+                    {d.img2Src ? (
+                      <div className="flex-shrink-0 sm:flex-none">
+                        <Image
+                          alt={d.title}
+                          src={d.img2Src}
+                          className="h-32 w-32 rounded-lg object-cover"
+                          width={54}
+                          height={54}
+                        />
+                      </div>
+                    ) : null}
+                    <div className="flex-1 text-center sm:text-left">
+                      <H3 withPadding={false} className="pb-3">
+                        {d.title}
+                      </H3>
+                      <p className="mb-4 text-gray-600 dark:text-gray-300">{d.description}</p>
                     </div>
-                  ) : null}
-                  <div className="flex-1 text-center sm:text-left">
-                    <H3 withPadding={false} className="pb-3">
-                      {d.title}
-                    </H3>
-                    <p className="mb-4 text-gray-600 dark:text-gray-300">{d.description}</p>
                   </div>
                 </div>
-              </div>
-            </Link>
-          </SectionContainer>
-        ))}
+              </Link>
+            </SectionContainer>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
