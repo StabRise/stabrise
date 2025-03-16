0dd1b88ca990536af808c9c29791dd1b2428a396
olchikd <olchikd@gmail.com>
2025-03-16 08:14:29 +0300

    Small changes in layout

--- components/SimpleCard.tsx
+++ components/SimpleCard.tsx
@@ -1,10 +1,10 @@
 const SimpleCard = ({ title, description, icon }) => (
   <div className="rounded-lg bg-white p-6 shadow-lg transition-shadow duration-300 hover:shadow-xl">
     {/* Icon and Title on the same line */}
-    <h4 className="mb-3 flex items-center text-xl font-semibold text-gray-600">
+    <h3 className="mb-3 flex items-center text-xl font-semibold text-gray-600">
       {icon && <span className="mr-2">{icon}</span>} {/* Add some margin for spacing */}
       {title}
-    </h4>
+    </h3>
     <p className="text-base text-gray-500">{description}</p>
   </div>
 )
