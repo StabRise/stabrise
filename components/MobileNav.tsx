b7666588a2c1cd8517ba738c11f4d6d2bc38b448
olchikd <olchikd@gmail.com>
2025-03-19 13:14:15 +0300

    Adding list of projects to the menu on mobile

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -73,14 +73,31 @@ const MobileNav = () => {
                 className="mt-8 flex h-full basis-0 flex-col items-start overflow-y-auto pt-2 pl-12 text-left"
               >
                 {headerNavLinks.map((link) => (
-                  <Link
-                    key={link.title}
-                    href={link.href}
-                    className="hover:text-primary-500 dark:hover:text-primary-400 mb-4 py-2 pr-4 text-2xl font-bold tracking-widest text-gray-900 outline outline-0 dark:text-gray-100"
-                    onClick={onToggleNav}
-                  >
-                    {link.title}
-                  </Link>
+                  <div key={link.title} className="mb-4">
+                    <Link
+                      key={link.title}
+                      href={link.href}
+                      className="hover:text-primary-500 dark:hover:text-primary-400 py-2 pr-4 text-2xl font-bold tracking-widest text-gray-900 outline outline-0 dark:text-gray-100"
+                      onClick={onToggleNav}
+                    >
+                      {link.title}
+                    </Link>
+
+                    {link.sublinks && (
+                      <div className="mt-2 ml-6 flex flex-col">
+                        {link.sublinks.map((sublink) => (
+                          <Link
+                            key={sublink.title}
+                            href={sublink.href}
+                            className="hover:text-primary-500 font-sm py-2 pr-4 text-xl text-gray-700 dark:text-gray-200"
+                            onClick={onToggleNav}
+                          >
+                            {sublink.title}
+                          </Link>
+                        ))}
+                      </div>
+                    )}
+                  </div>
                 ))}
               </nav>
 
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,18 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/blog', title: 'Blog' },
+  {
+    href: '/projects/',
+    title: 'Projects',
+    sublinks: [
+      { href: '/spark-pdf/', title: 'Spark PDF' },
+      { href: '/scaledp/', title: 'ScaleDP' },
+      { href: 'https://pdf-redaction.com/', title: 'PDF Redaction' },
+      { href: 'https://deidentify.online/', title: 'De-identify' },
+    ],
+  },
+  { href: '/blog/', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
+  { href: '/about/', title: 'About' },
 ]
 
 export default headerNavLinks
