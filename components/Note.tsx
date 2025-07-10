77998ccf685348d21d29b6081cc605dbcc6cedc7
olchikd <olchikd@gmail.com>
2025-07-10 08:32:57 +0200

    Updating theme switcher

--- components/Note.tsx
+++ components/Note.tsx
@@ -17,27 +17,13 @@ const Note: React.FC<NoteProps> = ({
 }) => {
   return (
     <div
-      className={`
-        bg-primary-50 border-l-4 border-blue-800 p-3 shadow-sm
-        text-md
-        text-gray-800
-        dark:bg-gray-900
-        dark:border-blue-600
-        dark:text-gray-200
-        ${className}
-      `}
+      className={`bg-primary-50 text-md border-l-4 border-blue-800 p-3 text-gray-800 shadow-sm dark:border-blue-600 dark:bg-gray-900 dark:text-gray-200 ${className} `}
     >
       <div className="flex items-start space-x-3">
         <span className="text-xl">{icon}</span>
         <div>
-          {title && (
-            <span className="font-semibold text-gray-800 dark:text-gray-100">
-              {title}
-            </span>
-          )}
-          {description && (
-            <p className="text-gray-600 dark:text-gray-300">{description}</p>
-          )}
+          {title && <span className="font-semibold text-gray-800 dark:text-gray-100">{title}</span>}
+          {description && <p className="text-gray-600 dark:text-gray-300">{description}</p>}
           <div>{children}</div>
         </div>
       </div>
